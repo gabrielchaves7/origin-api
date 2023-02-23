6c3809f94ff6b57eb64f038426dded4260f0c084
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-22 23:29:57 -0300

    Setup docker and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,18 @@ import { TaxController } from './tax/controller/tax.controller';
 import { TaxService } from './tax/service/tax.service';
 import { Tax } from './tax/entity/tax.entity';
 import { TaxModule } from './tax/tax.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'origin',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as unknown as number,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       entities: [Score, Tax],
       migrations: [],
