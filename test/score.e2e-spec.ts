b7c1235adf2755855f71e4a881695502c3282930
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-24 15:25:00 -0300

    Initial e2e test

---
+++ test/score.e2e-spec.ts
@@ -0,0 +1,45 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+describe('Score', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET score should return 200 when annualIncome and monthlyCosts are valid`, () => {
+    return request(app.getHttpServer())
+      .get('/score?annualIncome=1000&monthlyCosts=10')
+      .expect(200)
+      .then((response) => {
+        var score = response.body;
+        expect(score.status).toEqual('HEALTHY');
+        expect(score.monthlyCosts).toEqual(10);
+        expect(score.annualIncome).toEqual(1000);
+      });
+  });
+
+  it(`/GET score should return 400 when annualIncome is string`, () => {
+    return request(app.getHttpServer())
+      .get('/score?annualIncome="1000"&monthlyCosts=10')
+      .expect(400)
+  });
+
+  it(`/GET score should return 400 when monthlyCosts is string`, () => {
+    return request(app.getHttpServer())
+      .get('/score?annualIncome=1000&monthlyCosts="10"')
+      .expect(400)
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
